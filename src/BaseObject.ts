1d90949e800aab9de672a958119cc733a4a7dd4d
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2020-03-21 22:49:16 +0100

    Added API class and APIEndpoint classes

---
+++ src/BaseObject.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2018-2020 Gigadrive - All rights reserved.
+ * https://gigadrivegroup.com
+ * https://qpostapp.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://gnu.org/licenses/>
+ */
+
+import {JsonConvert} from "json2typescript";
+import {PropertyMatchingRule} from "json2typescript/src/json2typescript/json-convert-enums";
+
+export default class BaseObject {
+	private static jsonConvert: JsonConvert;
+
+	static convertObject<T>(type: (new () => T), object: string | object): T {
+		if (typeof object === "string") {
+			object = JSON.parse(object);
+		}
+
+		return this.getJsonConverter().deserializeObject(object, type);
+	}
+
+	static convertArray<T>(type: (new () => T), object: string | any[]): T[] {
+		let parsed: any[];
+
+		if (typeof object === "string") {
+			parsed = JSON.parse(object);
+		} else {
+			parsed = object;
+		}
+
+		return this.getJsonConverter().deserializeArray(parsed, type);
+	}
+
+	private static getJsonConverter(): JsonConvert {
+		if (!this.jsonConvert) {
+			this.jsonConvert = new JsonConvert();
+			this.jsonConvert.propertyMatchingRule = PropertyMatchingRule.CASE_INSENSITIVE;
+		}
+
+		return this.jsonConvert;
+	}
+}
